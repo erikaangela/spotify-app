bed06b41cf93414ba66da533e968971c062bc696
Erika <erikaangela4@gmail.com>
2022-07-21 13:50:25 -0700

    Working on SpotifyAuth with mentor code

--- src/components/SpotifyAuth.js
+++ src/components/SpotifyAuth.js
@@ -4,62 +4,27 @@
 // // const Spotify = require("spotify-web-api-js");
 // // const s = new Spotify();
 
-// const Buffer = require("buffer/").Buffer;
-// const qs = require("qs");
-
-// // const data = qs.stringify({ grant_type: "client_credentials" });
-
 // export const SpotifyAuth = async () => {
 //   //   const spotifyApi = new SpotifyWebApi();
 
-//   const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env;
-
-//   await axios
-//     .post(
-//       "https://accounts.spotify.com/api/token",
-//       qs.stringify({ grant_type: "client_credentials" }),
-//       {
-//         headers: {
-//           "Content-Type": "application/x-www-form-urlencoded",
-//           Authorization: `Basic ${Buffer.from(
-//             REACT_APP_CLIENT_ID + ":" + REACT_APP_CLIENT_SECRET
-//           ).toString("base64")}`,
-//         },
-//       }
-//     )
-//     .then((response) => {
-//       console.log(response.data.access_token);
-//     });
-// };
-
 import axios from "axios";
-import qs from "qs";
-
-const Buffer = require("buffer/").Buffer;
 
 export const getAuth = async () => {
-  const clientId = process.env.REACT_APP_CLIENT_ID;
-  const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-
-  const headers = {
-    headers: {
-      Accept: "application/json",
-      Authorization:
-        "Basic " + new Buffer(clientId + ":" + clientSecret).toString("base64"),
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  };
-  const data = {
-    grant_type: "client_credentials",
-  };
+  const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env;
 
   try {
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      qs.stringify(data),
-      headers
-    );
-    console.log(response.data.access_token);
+    const response = await axios("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          window.btoa(REACT_APP_CLIENT_ID + ":" + REACT_APP_CLIENT_SECRET),
+      },
+      data: "grant_type=client_credentials",
+    }).then((response) => {
+      console.log(response);
+    });
   } catch (error) {
     console.log(error);
   }
