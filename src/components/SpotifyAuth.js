e80c17416b680c3a835934cce8da9dda54470a9a
Erika <erikaangela4@gmail.com>
2022-07-22 17:41:10 -0700

    Fixing SpotifyAuth

--- src/components/SpotifyAuth.js
+++ src/components/SpotifyAuth.js
@@ -1,20 +1,32 @@
 import axios from "axios";
+import qs from "qs";
+
+const Buffer = require("buffer/").Buffer;
 
 export const getAuth = async () => {
-  const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env;
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+
+  const headers = {
+    Accept: "application/json",
+    Authorization:
+      "Basic " + new Buffer(clientId + ":" + clientSecret).toString("base64"),
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  const data = {
+    grant_type: "client_credentials",
+  };
 
   try {
     const response = await axios("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " +
-          window.btoa(REACT_APP_CLIENT_ID + ":" + REACT_APP_CLIENT_SECRET),
+        Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
       },
       data: "grant_type=client_credentials",
-    }).then((res) => {
-      console.log(res);
+    }).then((response) => {
+      console.log(response);
     });
   } catch (error) {
     console.log(error);
